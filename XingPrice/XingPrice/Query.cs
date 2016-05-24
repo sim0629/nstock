b1f6087d367f0b8312ae6846687f2b06f56bd920
Gyumin Sim <sim0629@gmail.com>
2016-05-24 14:34:45 +0900

    price more

--- XingPrice/XingPrice/Query.cs
+++ XingPrice/XingPrice/Query.cs
@@ -12,8 +12,8 @@ namespace XingPrice
     {
         private XAQuery query;
         private int year, month;
+        private string code;
 
-        private string code = "084990";
         private string in_block_name = "t8412InBlock";
         private string out_block_name = "t8412OutBlock1";
 
@@ -30,16 +30,17 @@ namespace XingPrice
             return query.GetFieldData(out_block_name, field_name, index);
         }
 
-        public Query(int year, int month)
+        public Query(string code, int year, int month)
         {
             query = (XAQuery)Activator.CreateInstance(Type.GetTypeFromProgID("XA_DataSet.XAQuery"));
             query.ResFileName = @"C:\eBEST\xingAPI\Res\t8412.res";
-            query.SetFieldData(in_block_name, "shcode", 0, code); // 바이로메드
-            query.SetFieldData(in_block_name, "ncnt", 0, "10");
+            query.SetFieldData(in_block_name, "shcode", 0, code);
+            query.SetFieldData(in_block_name, "ncnt", 0, "10"); // 10분
             query.SetFieldData(in_block_name, "comp_yn", 0, "N");
             query.ReceiveData += Receive_Handler;
             this.year = year;
             this.month = month;
+            this.code = code;
         }
 
         public int Run()
@@ -51,17 +52,30 @@ namespace XingPrice
 
         private void Receive_Handler(string szTrCode)
         {
-            using (var writer = new StreamWriter(String.Format("{0}-{1}{2}.txt", code, year, month.ToString("D2")), false, Encoding.UTF8))
+            var fn = String.Format("{0}-{1}{2}.txt", code, year, month.ToString("D2"));
+            using (var writer = new StreamWriter(fn, false, Encoding.ASCII))
             {
+                var prevDate = "";
                 var count = BlockCount;
                 for (var i = 0; i < count; i++)
                 {
                     var date = GetFieldData("date", i);
                     var time = GetFieldData("time", i);
-                    var open = GetFieldData("open", i);
-                    writer.WriteLine("{0} {1} {2}", date, time, open);
+                    var close = GetFieldData("close", i);
+                    if (date != prevDate)
+                    {
+                        var open = GetFieldData("open", i);
+                        var hours = Int32.Parse(time.Substring(0, 2));
+                        var minutes = Int32.Parse(time.Substring(2, 2));
+                        var val = hours * 60 + minutes - 10;
+                        hours = val / 60; minutes = val % 60;
+                        writer.WriteLine("{0} {1}{2}00 {3}", date, hours.ToString("D2"), minutes.ToString("D2"), open);
+                        prevDate = date;
+                    }
+                    writer.WriteLine("{0} {1} {2}", date, time, close);
                 }
             }
+            Console.WriteLine(fn);
         }
     }
 }
--- XingPrice/XingPrice/frmMain.cs
+++ XingPrice/XingPrice/frmMain.cs
@@ -14,6 +14,8 @@ namespace XingPrice
     public partial class frmMain : Form
     {
         private Session session;
+        private BackgroundWorker bw;
+        private bool working = false;
         
         public frmMain()
         {
@@ -22,6 +24,43 @@ namespace XingPrice
             FormClosed += frmMain_FormClosed;
 
             session = new Session(Login_Handler);
+            bw = new BackgroundWorker();
+            bw.DoWork += bw_DoWork;
+            bw.RunWorkerCompleted += bw_RunWorkerCompleted;
+        }
+
+        private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            working = false;
+            MessageBox.Show("done");
+        }
+
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var codes = new String[] {
+                "084990", // 바이로메드
+                "214370", // 케어젠
+                "102940", // 코오롱생명과학
+                "095700", // 제넥신
+                "034830", // 한국토지신탁
+                "046890", // 서울반도체
+            };
+
+            foreach (var code in codes)
+            {
+                for (var month = 5; month <= 12; month++)
+                {
+                    var query = new Query(code, 2015, month);
+                    if (query.Run() < 0) Console.WriteLine("Fail to query {0} {1}", 2015, month);
+                    Thread.Sleep(1200);
+                }
+                for (var month = 1; month <= 5; month++)
+                {
+                    var query = new Query(code, 2016, month);
+                    if (query.Run() < 0) Console.WriteLine("Fail to query {0} {1}", 2016, month);
+                    Thread.Sleep(1200);
+                }
+            }
         }
 
         private void frmMain_FormClosed(object sender, FormClosedEventArgs e)
@@ -53,18 +92,13 @@ namespace XingPrice
 
         private void btnRequest_Click(object sender, EventArgs e)
         {
-            for (var month = 5; month <= 12; month++)
+            if (working)
             {
-                var query = new Query(2015, month);
-                if (query.Run() < 0) Console.WriteLine("Fail to query {0} {1}", 2015, month);
-                Thread.Sleep(1200);
+                MessageBox.Show("working");
+                return;
             }
-            for (var month = 1; month <= 4; month++)
-            {
-                var query = new Query(2016, month);
-                if (query.Run() < 0) Console.WriteLine("Fail to query {0} {1}", 2016, month);
-                Thread.Sleep(1200);
-            }
+            working = true;
+            bw.RunWorkerAsync();
         }
     }
 }
